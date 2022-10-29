123e4b08d9e5e089976189c7282e14b2db5b45bc
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2022-10-29 18:06:56 +1100

    added alpinejs, responsive index page

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,14 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.0.0] - 2022-10-29
+### Added
+- Created global navbar
+- Created global footer
+- Created base index page
+- Recreated the circles graphic
+- Added alpinejs integration
+
 ## [1.0.0] - 2022-10-21
 ### Added
 - Created base AstroJS project
--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import tailwind from "@astrojs/tailwind";
 import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://swinlead.com',
-  integrations: [sitemap(), tailwind(), robotsTxt()]
+  integrations: [sitemap(), tailwind(), robotsTxt(), alpinejs()]
 });
